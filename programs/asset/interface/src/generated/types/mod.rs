891edf32ca20772d8167b8218ee35424cb030a20
febo <febo@tensor.so>
2024-05-11 23:47:23 -0400

    Update program interface

--- programs/asset/interface/src/generated/types/mod.rs
+++ programs/asset/interface/src/generated/types/mod.rs
@@ -9,10 +9,12 @@ pub(crate) mod r#delegate_input;
 pub(crate) mod r#delegate_role;
 pub(crate) mod r#extension_input;
 pub(crate) mod r#extension_type;
+pub(crate) mod r#size_input;
 pub(crate) mod r#standard;
 
 pub use self::r#delegate_input::*;
 pub use self::r#delegate_role::*;
 pub use self::r#extension_input::*;
 pub use self::r#extension_type::*;
+pub use self::r#size_input::*;
 pub use self::r#standard::*;
