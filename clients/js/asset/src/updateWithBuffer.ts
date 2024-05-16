9e617d49224b9b63c81b1ab1b70083ea623f6648
febo <febo@tensor.so>
2024-05-16 17:51:01 -0400

    Add proxy support

--- clients/js/asset/src/updateWithBuffer.ts
+++ clients/js/asset/src/updateWithBuffer.ts
@@ -1,10 +1,11 @@
 import {
   Context,
+  PublicKey,
   TransactionBuilderGroup,
   generateSigner,
   transactionBuilderGroup,
 } from '@metaplex-foundation/umi';
-import { SYSTEM_PROGRAM_ID } from '.';
+import { ASSET_PROGRAM_ID, SYSTEM_PROGRAM_ID, close } from '.';
 import { TypedExtension, getExtensionSerializerFromType } from './extensions';
 import {
   UpdateInstructionAccounts,
@@ -23,15 +24,40 @@ export function updateWithBuffer(
     Omit<UpdateInstructionArgs, 'extension'> & {
       extension: TypedExtension;
       chunkSize?: number;
-    }
+    } & { proxy?: PublicKey }
 ): TransactionBuilderGroup {
   const data = getExtensionSerializerFromType(input.extension.type).serialize(
     input.extension
   );
   const chunked = data.length > (input.chunkSize ?? DEFAULT_CHUNK_SIZE);
 
+  if (input.proxy) {
+    const proxied = context.programs.clone();
+    proxied.bind('asset', input.proxy);
+    context = { ...context, programs: proxied };
+  }
+
   if (chunked) {
     const buffer = generateSigner(context);
+
+    let updateIx = update(context, {
+      ...input,
+      buffer: buffer.publicKey,
+      systemProgram: SYSTEM_PROGRAM_ID,
+      extension: {
+        extensionType: input.extension.type,
+        length: data.length,
+        data: null,
+      },
+    });
+
+    if (input.proxy) {
+      updateIx = updateIx.addRemainingAccounts({
+        pubkey: ASSET_PROGRAM_ID,
+        isWritable: false,
+        isSigner: false,
+      });
+    }
 
     return write(context, {
       ...input,
@@ -49,28 +75,31 @@ export function updateWithBuffer(
           },
         })
       )
+      .append(updateIx)
       .append(
-        update(context, {
-          ...input,
-          buffer: buffer.publicKey,
-          systemProgram: SYSTEM_PROGRAM_ID,
-          extension: {
-            extensionType: input.extension.type,
-            length: data.length,
-            data: chunked ? null : data,
-          },
+        close(context, {
+          buffer,
+          recipient: input.payer?.publicKey ?? context.payer.publicKey,
         })
       );
   }
 
-  return transactionBuilderGroup([
-    update(context, {
-      ...input,
-      extension: {
-        extensionType: input.extension.type,
-        length: data.length,
-        data,
-      },
-    }),
-  ]);
+  let updateIx = update(context, {
+    ...input,
+    extension: {
+      extensionType: input.extension.type,
+      length: data.length,
+      data,
+    },
+  });
+
+  if (input.proxy) {
+    updateIx = updateIx.addRemainingAccounts({
+      pubkey: ASSET_PROGRAM_ID,
+      isWritable: false,
+      isSigner: false,
+    });
+  }
+
+  return transactionBuilderGroup([updateIx]);
 }
