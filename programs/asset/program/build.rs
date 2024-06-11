610047cb49dbd21c2dae480b5377d4b10cb9ca52
ngundotra <noah@gundotra.org>
2024-06-11 23:56:58 +0900

    use include-idl to write idl to asset program

---
+++ programs/asset/program/build.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+use include_idl::compress_idl;
+
+fn main() {
+    // Run shank to generate the IDL
+    let _output = Command::new("shank")
+        .arg("idl")
+        .arg("-o")
+        .arg("../../../idls")
+        .output()
+        .expect("Failed to run shank");
+
+    // Get the IDL path
+    let idl_path = PathBuf::from("../../../idls").join("asset_program.json");
+
+    // Concat output path of compressed IDL
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = PathBuf::from(out_dir).join("idl.json.zip");
+
+    compress_idl(&idl_path, &dest_path);
+}
